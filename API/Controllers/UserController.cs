c466c78c0df11a9a57f8712d2dab46818f8b9962
saahilanande <saahilanande21@gmail.com>
2023-10-01 19:59:36 -0500

    User Controller added

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+
+    private readonly DataContext dataContext;
+
+    public UserController(DataContext dataContext)
+    {
+        this.dataContext = dataContext;
+    }
+
+
+    //Endpoint to get all users
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        return dataContext.Users.ToList();
+    }
+
+    //Endpoint to get a user by Id
+    [HttpGet("{id}")]
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        return dataContext.Users.Find(id);
+    }
+
+}
