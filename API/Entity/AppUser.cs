ca6a6921d35bf0b3b8d82f2bb4c2471af8c05a43
saahilanande <saahilanande21@gmail.com>
2023-10-06 18:56:07 -0500

    DTO added and update the migration for validation

--- API/Entity/AppUser.cs
+++ API/Entity/AppUser.cs
@@ -1,11 +1,15 @@
-﻿namespace API;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API;
 
 public class AppUser
 {
     public int Id { get; set; }
 
+    [Required]
     public string UserName { get; set; }
 
+    [Required]
     public byte[] passwordHash {get; set;}
 
     public byte[] passwordSalt { get; set; }
