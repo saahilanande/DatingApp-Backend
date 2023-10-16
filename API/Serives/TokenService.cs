f7d2c3eabc8b353e76c12e4397c43917ed84474d
saahilanande <saahilanande21@gmail.com>
2023-10-16 18:37:34 -0500

    token interface and service class added

---
+++ API/Serives/TokenService.cs
@@ -0,0 +1,12 @@
+using API.Interfaces;
+
+namespace API.Serives;
+
+public class TokenService : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        throw new NotImplementedException();
+    }
+
+}
