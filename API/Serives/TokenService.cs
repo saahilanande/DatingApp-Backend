617ad7b607476172ebe4f2141f5965dc9641152a
saahilanande <saahilanande21@gmail.com>
2023-10-16 18:55:46 -0500

    Token logic added in service

--- API/Serives/TokenService.cs
+++ API/Serives/TokenService.cs
@@ -1,12 +1,43 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Serives;
 
 public class TokenService : ITokenService
 {
+
+    private readonly SymmetricSecurityKey _key;
+
+    public TokenService(IConfiguration configuration){
+
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenKey"]));
+
+    }
+
+
+
     public string CreateToken(AppUser user)
     {
-        throw new NotImplementedException();
+        var claims = new List<Claim>{
+            new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+        };
+
+        var creds = new SigningCredentials(_key,SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescription = new SecurityTokenDescriptor{
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(6),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var token = tokenHandler.CreateToken(tokenDescription);
+
+        return tokenHandler.WriteToken(token);
     }
 
 }
